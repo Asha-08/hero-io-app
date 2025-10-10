608f727016855de396ddc705d3bc26b85ae52606
Asha-08 <ashaamina91@gmail.com>
2025-10-10 17:21:31 +0600

    final commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router'
-import App from './App'
+
 import { router } from './Routes/Routes'
 
 
